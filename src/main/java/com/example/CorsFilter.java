b639297ece6fc0007e91edb02a52fc10dd5ad3af
Saurabh43 <Saurabh43@users.noreply.github.com>
2020-11-10 20:43:42 -0800

    Update CorsFilter.java

--- src/main/java/com/example/CorsFilter.java
+++ src/main/java/com/example/CorsFilter.java
@@ -18,7 +18,7 @@ public class CorsFilter implements Filter {
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
-		response.setHeader("Access-Control-Allow-Origin", "*");
+		//response.setHeader("Access-Control-Allow-Origin", "*");
 	}
 
 	public void destroy() {
