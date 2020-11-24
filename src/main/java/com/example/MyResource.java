e11ac074d8421f6d69637a9dc22ef4794ccb8102
Saurabh43 <Saurabh43@users.noreply.github.com>
2020-11-23 23:39:28 -0800

    Update MyResource.java

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -30,13 +30,13 @@ public class MyResource {
         return dbUrl;
     }
     
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//     private static Connection getConnection() throws URISyntaxException, SQLException {
+//         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+//         String username = dbUri.getUserInfo().split(":")[0];
+//         String password = dbUri.getUserInfo().split(":")[1];
+//         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
 
-        return DriverManager.getConnection(dbUrl, username, password);
-    }
+//         return DriverManager.getConnection(dbUrl, username, password);
+//     }
 }
